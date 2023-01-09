a0eae95f41347b04334fa13a1f81a385e2303fa6
goyois <bau12288@gmail.com>
2023-01-09 20:09:37 +0900

    [TIL #4]: 주석 변경

--- src/main/java/til/java/basic/console/In.java
+++ src/main/java/til/java/basic/console/In.java
@@ -6,7 +6,7 @@ public class In {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);  //스캐너 클래스의 인스턴스르 생성
-        String inputValue = sc.nextLine();  //입력한 내용이 inputValue 에 저장됨
+        String inputValue = sc.nextLine();  //nextLine() 콘솔을 통해 입력받 기능을 수행하고 입력된 콘솔에 입력된 값은 변수에 할당됨
 
         System.out.println(inputValue);  //입력한 문자열이 출력됩니다.
     }
